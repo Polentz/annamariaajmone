159df94a760785124ef6cfc93ef3afe2b3522a9e
Giulia Polenta <giuliapolenta@gmail.com>
2023-01-11 10:41:20 +0100

    new arcipelago page

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // general // document height
 const documentHeight = () => {
-    const doc = document.documentElement
-    doc.style.setProperty("--doc-height", `${window.innerHeight}px`)
-}
+    const doc = document.documentElement;
+    doc.style.setProperty("--doc-height", `${window.innerHeight}px`);
+};
 window.addEventListener("resize", documentHeight)
 documentHeight();
 
@@ -12,7 +12,6 @@ if (anchorTags) {
     anchorTags.forEach(link => {
         link.addEventListener("click", (event) => {
             event.preventDefault();
-
             const href = link.getAttribute("href");
             document.querySelector(href).scrollIntoView({
                 behavior: "smooth"
@@ -76,4 +75,49 @@ if (spinBanner) {
     spinBanner.addEventListener("mouseenter", () => {
         spinBanner.style.transform = "rotate(" + 0 + "deg)";
     })
+}
+
+// special // arcipelago
+const projectLinks = document.querySelectorAll(".grid-arc .link");
+const projects = document.querySelectorAll(".project");
+const body = document.querySelector("body");
+const pageBtn = document.querySelector(".page-title");
+const pageInfo = document.querySelector(".page-info");
+const infoClose = document.querySelector(".info-close");
+if (projects) {
+    projectLinks.forEach(link => {
+        link.addEventListener("click", (event) => {
+            event.preventDefault();
+            const href = link.getAttribute("href");
+            if (projects) {
+                projects.forEach(project => {
+                    if (href === `#${project.id}`) {
+                        project.style.display = "block";
+                        project.scrollIntoView({
+                            behavior: "smooth"
+                        });
+                        setTimeout(() => {
+                            body.style.overflow = "hidden"
+                        }, 1000);
+                    }
+                    const projectClose = document.querySelectorAll(".project-close");
+                    projectClose.forEach(btnClose => {
+                        btnClose.addEventListener("click", () => {
+                            project.style.display = "none";
+                        });
+                    });
+                });
+            }
+            document.querySelector(href).scrollIntoView({
+                behavior: "smooth"
+            });
+        });
+    });
+
+    pageBtn.addEventListener("click", () => {
+        pageInfo.classList.add("open");
+    });
+    infoClose.addEventListener("click", () => {
+        pageInfo.classList.remove("open");
+    });
 }
